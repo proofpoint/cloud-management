1d50e6938a457c90bf4485eeb9959015e55292fd
Mark Kent <mkent@proofpoint.com>
2011-10-30 20:35:54 -0700

    Provide mock inventory server implementation; enable unit tests for inventory client

--- src/test/java/com/proofpoint/cloudmanagement/service/inventoryclient/TestClient.java
+++ src/test/java/com/proofpoint/cloudmanagement/service/inventoryclient/TestClient.java
@@ -19,23 +19,29 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.proofpoint.cloudmanagement.service.inventoryclient.MockInventoryServer.MockInventoryServerModule;
 import com.proofpoint.configuration.ConfigurationFactory;
 import com.proofpoint.configuration.ConfigurationModule;
+import com.proofpoint.http.server.testing.TestingHttpServer;
+import com.proofpoint.http.server.testing.TestingHttpServerModule;
+import com.proofpoint.jaxrs.JaxrsModule;
 import com.proofpoint.json.JsonModule;
+import com.proofpoint.node.testing.TestingNodeModule;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import javax.ws.rs.core.UriBuilder;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
 public class TestClient
 {
     private InventoryClient client;
+    private TestingHttpServer server;
+    private MockInventoryServer inventoryServer;
 
-    private static final String SYSTEM_NAME = "name";
-    private static final String OPENSTACK_ID = "id";
-    private static final String INVENTORY_BASE_URI = "http://localhost/api";
     private static final String INVENTORY_USER = "readonly";
     private static final String INVENTORY_PASSWORD = "readonly";
 
@@ -43,30 +49,53 @@ public class TestClient
     public void setup()
             throws Exception
     {
+        Injector inventoryServerInjector = Guice.createInjector(
+                new TestingHttpServerModule(),
+                new TestingNodeModule(),
+                new JaxrsModule(),
+                new JsonModule(),
+                new MockInventoryServerModule()
+        );
+
+        server = inventoryServerInjector.getInstance(TestingHttpServer.class);
+        inventoryServer = inventoryServerInjector.getInstance(MockInventoryServer.class);
+        server.start();
+
         Injector injector = Guice.createInjector(
                 new JsonModule(),
                 new InventoryClientModule(),
                 new ConfigurationModule(
                         new ConfigurationFactory(
                                 ImmutableMap.<String, String>builder()
-                                        .put("inventory.user", INVENTORY_USER)
-                                        .put("inventory.password", INVENTORY_PASSWORD)
-                                        .put("inventory.base-uri", INVENTORY_BASE_URI)
+                                        .put("inventory.user",
+                                                INVENTORY_USER)
+                                        .put("inventory.password",
+                                                INVENTORY_PASSWORD)
+                                        .put("inventory.base-uri",
+                                                UriBuilder.fromUri(server.getBaseUrl())
+                                                        .path("/inv_api/v1")
+                                                        .build()
+                                                        .toString())
                                         .build())));
 
         client = injector.getInstance(InventoryClient.class);
     }
 
 
-    @Test(enabled = false)
+    @Test
     public void testGetSystem()
             throws Exception
     {
-        InventorySystem system = client.getSystem(SYSTEM_NAME);
-        Assert.assertEquals(system.getSerialNumber(), OPENSTACK_ID);
+        String id = UUID.randomUUID().toString();
+
+        Map<String, String> response = inventoryServer.getPcmName(id);
+        String name = response.get("fqdn");
+
+        InventorySystem system = client.getSystem(name);
+        Assert.assertEquals(system.getSerialNumber(), id);
     }
 
-    @Test(enabled = false)
+    @Test
     public void testGetMissingSystem()
             throws Exception
     {
@@ -74,19 +103,23 @@ public class TestClient
         Assert.assertNull(system);
     }
 
-    @Test(enabled = false)
+    @Test
     public void testSetSystemRoles()
             throws Exception
     {
+        String id = UUID.randomUUID().toString();
+        Map<String, String> response = inventoryServer.getPcmName(id);
+        String name = response.get("fqdn");
+
         Set<String> roles = ImmutableSet.of(UUID.randomUUID().toString(), UUID.randomUUID().toString());
 
-        client.setSystemRoles(SYSTEM_NAME, roles);
+        client.setSystemRoles(name, roles);
 
-        InventorySystem updated = client.getSystem(SYSTEM_NAME);
+        InventorySystem updated = client.getSystem(name);
         Assert.assertEquals(updated.getRoles(), roles);
     }
 
-    @Test(enabled = false)
+    @Test
     public void testSetSystemRolesForMissingSystem()
             throws Exception
     {
@@ -102,16 +135,17 @@ public class TestClient
         Assert.fail("Expected an exception to be thrown");
     }
 
-    @Test(enabled = false)
+    @Test
     public void testPcmSystemName()
             throws Exception
     {
-        String name = client.getPcmSystemName(OPENSTACK_ID);
-        Assert.assertEquals(name, SYSTEM_NAME);
+        String id = UUID.randomUUID().toString();
+
+        String name = client.getPcmSystemName(id);
 
         // check get same value twice.
-        name = client.getPcmSystemName(OPENSTACK_ID);
-        Assert.assertEquals(name, SYSTEM_NAME);
+        String nameAgain = client.getPcmSystemName(id);
+        Assert.assertEquals(name, nameAgain);
     }
 
     @Test
